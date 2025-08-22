569f9d7f09b92e1834d2a4fdb3ceb6ffc55363da
eobabs <eobabs@gmail.com>
2025-08-22 13:05:33 +0100

    Authentication flow completed

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./src/config/databaseConfig');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./src/routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,3 +32,32 @@ exports.register = async (req, res) => {
         res.status(500).send('Server error');
     }
 };
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({msg: 'Invalid credentials'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({msg: 'Invalid credentials'});
+        }
+
+        const payload = {user: {id: user.id, role: user.role}};
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {expiresIn: '5h'},
+            (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            });
+    } catch (error){
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+};
+
