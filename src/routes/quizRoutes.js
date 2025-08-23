a87bf6e7852cc8b75c7783ebf7b49fabee2595cf
eobabs <eobabs@gmail.com>
2025-08-23 20:04:14 +0100

    Edit and Delete Quiz route added- updated with routes

--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
-const { createQuiz, getQuizzesByTeacher, getQuizById } = require('../controllers/quizController');
+const { createQuiz, getQuizzesByTeacher, getQuizById, updateQuiz, deleteQuiz } = require('../controllers/quizController');
 
 
 router.post('/', auth, createQuiz);
 router.get('/', auth, getQuizzesByTeacher);
 router.get('/:quizId', auth, getQuizById);
-
+router.put('/:quizId', auth, updateQuiz);
+router.delete('/:quizId', auth, deleteQuiz);
 
 module.exports = router;
