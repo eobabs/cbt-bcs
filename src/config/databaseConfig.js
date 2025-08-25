ef3c22b382b50b6c430cc0955c8b7e26f2b5beff
eobabs <eobabs@gmail.com>
2025-08-25 20:15:19 +0100

    Fixed DB Config after Tests Batch1- Auth Register/Quizzes Update&delete API

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -6,9 +6,9 @@ dotenv.config();
 const connectDatabase = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        if(process.env.NODE_ENV !== 'test') {
+
             console.log('MongoDB connected successfully.');
-        }
+
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
         process.exit(1);
