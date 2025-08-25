b61895ff4c7c9ec16397a49841609f043fb2919f
eobabs <eobabs@gmail.com>
2025-08-25 20:59:39 +0100

    Fixed DB Config check

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -6,12 +6,16 @@ dotenv.config();
 const connectDatabase = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-
+        if (process.env.NODE_ENV !== 'test') {
             console.log('MongoDB connected successfully.');
-
+        }
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
-        process.exit(1);
+        if (process.env.NODE_ENV !== 'test') {
+            process.exit(1);
+        } else {
+            throw error;
+        }
     }
 };
 
